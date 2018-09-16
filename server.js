8d78f0716c3b8bb9823dbdcffced50bb974ef8a5
Glitch (broken-stallion) <none>
2018-09-16 10:10:02 +0000

    🚡🚜 Checkpoint ./package.json:1589743/5435 ./server.js:1589743/257

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 
 // init project
 var express = require('express');
+var bodyParser = require('body-parser');
+var dataStore = require('nedb');
+var cors = require('cors');
 var app = express();
 
 // we've started you off with Express, 
@@ -10,6 +13,10 @@ var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.json());
+
+var db = new dataStore({filename: 'path/to/file.json', autoload: true});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
