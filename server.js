9e05c3f0106c56d241543336bd133eca3beb095a
kankuu <akhmadbasir5@gmail.com>
2018-09-19 00:03:39 +0700

    modified:   server.js add handler

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(cors());
 app.use(bodyParser.json());
 momentJS.locale('id');
 
-var db = new dataStore({filename: 'path/to/file.json', autoload: true});
+var db = new dataStore({
+  filename: 'path/to/file.json',
+  autoload: true
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 /**
@@ -27,49 +30,77 @@ app.get('/', function(request, response) {
 });
 */
 
-app.get('/', (req,res)=>{
-  db.find({}, (err,data)=>{
-    if(err)
+app.get('/', (req, res) => {
+  db.find({}, (err, data) => {
+    if (err)
       throw err;
     else
       res.send(data);
   })
 })
 
-app.post('/', (req,res)=>{
+app.post('/', (req, res) => {
   let day = momentJS().format('LLLL');
-  let doc = { ...req.body, day };
-  db.insert(doc,(err,data)=>{
-    if(err)
-      throw err
-    else
-      res.send('data has posted')
-  })
+  let doc = { ...req.body,
+    day
+  };
+  // empty handle data
+  if (
+    req.body.name === '' ||
+    req.body.nim === '' ||
+    req.body.phone === '' ||
+    req.body.gender === '' ||
+    req.body.email === '' ||
+    req.body.faculty === ''
+  ) {
+    res.json({
+      status_code : 301,
+      message : 'please fill all form!'
+    })
+  } else {
+    db.insert(doc, (err, data) => {
+      if (err)
+        throw err
+      else
+        res.json({
+          status_code: 200,
+          message : 'data saved!'
+        })
+    })
+  }
 })
 
-app.get('/:id', (req,res)=>{
-  db.findOne({_id: req.params.id}, {}, (err,data)=>{
-    if(err)
+app.get('/:id', (req, res) => {
+  db.findOne({
+    _id: req.params.id
+  }, {}, (err, data) => {
+    if (err)
       throw err
     else
       res.send(data)
   })
 })
 
-app.put('/:id',(req,res)=>{
+app.put('/:id', (req, res) => {
   let day = momentJS().format('LLLL');
-  let doc = { ...req.body, day };
-  db.update({_id: req.params.id}, doc, {}, (err,data)=>{
-    if(err)
+  let doc = { ...req.body,
+    day
+  };
+  db.update({
+    _id: req.params.id
+  }, doc, {}, (err, data) => {
+    if (err)
       throw err
     else
       res.send('Your data has been updated')
   })
 })
 
-app.delete('/:id', (req,res)=>{
-  db.remove({_id: req.params.id}, {}, (err,data)=>{
-    if(err)
+app.delete('/:id', (req, res) => {
+  db.remove({
+    _id: req.params.id
+  }, {}, (err, data) => {
+    if (err)
       throw err
     else
       res.send('Your data has been deleted')
@@ -77,6 +108,6 @@ app.delete('/:id', (req,res)=>{
 })
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
